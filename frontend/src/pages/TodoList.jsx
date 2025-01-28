43b892edde4e36481adf3ba5444fbdb3f7bd6e7a
niresh <nireshine@gmail.com>
2025-01-28 14:52:36 +0530

    added pagination to todoLiast

--- frontend/src/pages/TodoList.jsx
+++ frontend/src/pages/TodoList.jsx
@@ -1,6 +1,18 @@
 import { AgGridReact } from "ag-grid-react";
-import { ModuleRegistry } from "ag-grid-community";
-import { ClientSideRowModelModule } from "ag-grid-community";
+import {
+  ModuleRegistry,
+  PaginationModule,
+  ValidationModule,
+  createGrid,
+  ClientSideRowModelModule,
+} from "ag-grid-community";
+
+import {
+  ColumnMenuModule,
+  ColumnsToolPanelModule,
+  ContextMenuModule,
+  ServerSideRowModelModule,
+} from "ag-grid-enterprise";
 import { useState, useEffect, useContext } from "react";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import { AppContent } from "../context/app.context";
@@ -8,15 +20,21 @@ import SidePanel from "../components/SidePanel";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-ModuleRegistry.registerModules([ClientSideRowModelModule]);
+ModuleRegistry.registerModules([
+  PaginationModule,
+  ColumnsToolPanelModule,
+  ColumnMenuModule,
+  ContextMenuModule,
+  ServerSideRowModelModule,
+  ValidationModule,
+  ClientSideRowModelModule
+]);
 
 const TodoList = () => {
   const { getTodosList, todoslist, backendURL } = useContext(AppContent);
   const [rowData, setRowData] = useState([]);
-  const [selectedTodo, setSelectedTodo] = useState(null);  // State for selected todo
-
+  const [selectedTodo, setSelectedTodo] = useState(null); // State for selected todo
   const [isModalOpen, setIsModalOpen] = useState(false);
-
 
   // Define column definitions for the grid
   const [colDefs] = useState([
@@ -27,7 +45,6 @@ const TodoList = () => {
     { field: "createdAt", headerName: "Created on", sortable: true, filter: true },
     { field: "updatedAt", headerName: "Updated on", sortable: true, filter: true },
     { field: "description", headerName: "Description" },
-    // { field: "id", headerName: "ID" },
   ]);
 
   useEffect(() => {
@@ -46,7 +63,7 @@ const TodoList = () => {
       description: todo.description,
       createdAt: new Date(todo.createdAt).toLocaleString(),
       updatedAt: new Date(todo.updatedAt).toLocaleString(),
-      id: todo.id
+      id: todo.id,
     }));
     setRowData(transformedData);
   }, [todoslist]);
@@ -54,38 +71,32 @@ const TodoList = () => {
   // Handler for click event
   const onRowClick = (event) => {
     setSelectedTodo(event.data);
-    console.log("selectedTodo",event.data)
-    setIsModalOpen(true)
+    setIsModalOpen(true);
   };
 
-  const onClickCancel = (event) => {
-    setSelectedTodo(event.data);
-    setIsModalOpen(false)
+  const onClickCancel = () => {
+    setSelectedTodo(null);
+    setIsModalOpen(false);
   };
 
-  const onClickDelete =  async (event) => {
+  const onClickDelete = async (event) => {
     event.preventDefault();
-    // setIsModalOpen(false)
-    const todoId = selectedTodo.id
-    console.log("backendURL",`${backendURL}/api/todo/${todoId}`)
-    console.log("todoId",todoId)
+    const todoId = selectedTodo.id;
     try {
-      const {data} = await axios.delete(`${backendURL}/api/todo/${todoId}`);
-      console.log("$$$,deleteTodo",data)
+      const { data } = await axios.delete(`${backendURL}/api/todo/${todoId}`);
       if (data.success) {
         toast.success("Todo deleted successfully!");
         setIsModalOpen(false);
+      } else {
+        toast.error("Failed to delete Todo");
       }
-      else {
-       toast.error("Failed to deeeeeeeeeelete Todo");
-        }
     } catch (error) {
       toast.error(error.message);
     }
   };
 
   return (
-    <div className='flex h-screen overflow-hidden'>
+    <div className="flex h-screen overflow-hidden">
       <SidePanel />
       <div className="TodoList py-24 w-full">
         <h1 className="text-4xl p-3">TodoList..!!</h1>
@@ -98,8 +109,18 @@ const TodoList = () => {
             rowData={rowData}
             columnDefs={colDefs}
             animateRows={true}
-            defaultColDef={{ resizable: true }} // resizable
+            defaultColDef={{
+              resizable: true,
+              cellStyle: { padding: "20px" },
+            }}
+            rowStyle={{
+              cursor: "pointer",
+              transition: "background-color 0.3s ease",
+            }}
             onRowClicked={onRowClick}
+            pagination={true}
+            paginationPageSize={20}
+            suppressPaginationPanel={false}
           />
         </div>
 
@@ -107,87 +128,63 @@ const TodoList = () => {
         {selectedTodo && (
           <div className="todo-detail-form fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
             <div className="bg-white rounded-lg p-6 w-96 shadow-lg">
-            <h2 className="text-lg font-bold mb-4">Todo Details</h2>
-            <form>
-              <div>
-                <label>Title: </label>
-                <input
-                  type="text"
-                  value={selectedTodo.title}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label>Status: </label>
-                <input
-                  type="text"
-                  value={selectedTodo.status}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label>Owner: </label>
-                <input
-                  type="text"
-                  value={selectedTodo.owner}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label>Due Date: </label>
-                <input
-                  type="text"
-                  value={selectedTodo.due}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label>Description: </label>
-                <input
-                  type="text"
-                  value={selectedTodo.description}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label>Created On: </label>
-                <input
-                  type="text"
-                  value={selectedTodo.createdAt}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label>Updated On: </label>
-                <input
-                  type="text"
-                  value={selectedTodo.updatedAt}
-                  readOnly
-                />
-              </div>
-              <div className="flex justify-end">
-                <button
-                  type="button"
-                  onClick={onClickDelete}
-                  className="mr-2 px-4 py-2 rounded-lg bg-red-400 hover:bg-red-500"
-                >
-                  Delete
-                </button>
-                <button
-                  type="button"
-                  onClick={onClickCancel}
-                  className="mr-2 px-4 py-2 bg-gray-300 rounded-lg hover:bg-gray-400"
-                >
-                  Cancel
-                </button>
-                <button
-                  type="submit"
-                  className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-                >
-                  edit/Save
-                </button>
+              <h2 className="text-lg font-bold mb-4">Todo Details</h2>
+              <form>
+                <div>
+                  <label>Title: </label>
+                  <input type="text" value={selectedTodo.title} readOnly />
                 </div>
-            </form>
+                <div>
+                  <label>Status: </label>
+                  <input type="text" value={selectedTodo.status} readOnly />
+                </div>
+                <div>
+                  <label>Owner: </label>
+                  <input type="text" value={selectedTodo.owner} readOnly />
+                </div>
+                <div>
+                  <label>Due Date: </label>
+                  <input type="text" value={selectedTodo.due} readOnly />
+                </div>
+                <div>
+                  <label>Description: </label>
+                  <input
+                    type="text"
+                    value={selectedTodo.description}
+                    readOnly
+                  />
+                </div>
+                <div>
+                  <label>Created On: </label>
+                  <input type="text" value={selectedTodo.createdAt} readOnly />
+                </div>
+                <div>
+                  <label>Updated On: </label>
+                  <input type="text" value={selectedTodo.updatedAt} readOnly />
+                </div>
+                <div className="flex justify-end">
+                  <button
+                    type="button"
+                    onClick={onClickDelete}
+                    className="mr-2 px-4 py-2 rounded-lg bg-red-400 hover:bg-red-500"
+                  >
+                    Delete
+                  </button>
+                  <button
+                    type="button"
+                    onClick={onClickCancel}
+                    className="mr-2 px-4 py-2 bg-gray-300 rounded-lg hover:bg-gray-400"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+                  >
+                    Edit/Save
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         )}
